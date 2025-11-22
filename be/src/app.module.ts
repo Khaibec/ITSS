9b99a389ce6642dd9a00f50b92a63b04af7383d4
PL <mycloud1235@gmail.com>
2025-11-22 22:37:42 +0700

    auth and chat_sending

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ChatModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
